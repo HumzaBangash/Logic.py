5c4d754690e42c641cee40be4eec562841829b6b
HumzaBangash <hbangash8222@students.mchenry.edu>
2026-02-11 19:04:02 -0600

    Add files via upload

---
+++ Logic_HumzaBangash.py
@@ -0,0 +1,14 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included with your name.
+[ ] 2. Ask user for two integers (num1 and num2).
+[ ] 3. Perform 6 logical checks: (Both > 0, Both > 100, Either Even, Either < 100, Not Equal, Not Zero).
+[ ] 4. Use if/elif/else to categorize num1 (Positive/Negative/Zero).
+[ ] 5. Code is clean and uses descriptive variable names.
+[ ] 6. Upload to GitHub and paste the link below.
+-----------------------------------------------------------------------
+"""
+Integer_1 = int(input(f"Please enter a whole number: "))
+Integer_2 = int(input(f"Please enter a different whole number: "))
